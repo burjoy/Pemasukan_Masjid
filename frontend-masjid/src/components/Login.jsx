93b7bde5fb3742d4a9a6c94cf35c13adaf8194a5
burjoy <luqman.hadi85@gmail.com>
2026-01-19 13:32:23 +0700

    feat: added login placeholder

---
+++ frontend-masjid/src/components/Login.jsx
@@ -0,0 +1,16 @@
+function Login(){
+    return(
+        <>
+        <div>
+            <h1>Login Page</h1>
+            <input type="text" placeholder="Username" />
+            <br />
+            <input type="password" placeholder="Password" />
+            <br />
+            <button>Login</button>
+        </div>
+        </>
+    )
+}
+
+export default Login;
