ec5393219b1fe18ad2353d8280c49756057f86ba
burjoy <luqman.hadi85@gmail.com>
2026-03-07 09:52:02 +0700

    feat: added google drive connector and helper function

--- server/init/googleAuth.js
+++ server/init/googleAuth.js
@@ -3,6 +3,7 @@ const { google } = require('googleapis');
 const credentials = JSON.parse(process.env.SERVICE_ACC_CREDS);
 
 let sheetsClient = null;
+let driveClient = null;
 
 async function initGoogleSheets() {
     if (sheetsClient) return sheetsClient; // Return existing client if already initialized
@@ -34,4 +35,31 @@ function getSheetsClient() {
     return sheetsClient;
 }
 
-module.exports = { initGoogleSheets, getSheetsClient };
+// Function for google drive initialization
+async function initGoogleDrive() {
+    try {
+        const auth = new google.auth.GoogleAuth({
+            credentials,
+            scopes: ['https://www.googleapis.com/auth/drive.readonly'], // Use read-write scope if needed
+        });
+
+        const client = await auth.getClient();
+
+        // store client globally
+        driveClient = google.drive({ version: 'v3', auth: client });
+        console.log('✅ Google Drive API successfully authenticated.');
+        return driveClient;
+    } catch (error) {
+        console.error('❌ Error initializing Google Drive:', error);
+        throw error;
+    }
+}
+
+function getDriveClient() {
+    if (!driveClient) {
+        throw new Error("Drive client not initialized. Call initGoogleDrive() first.");
+    }
+    return driveClient;
+}
+
+module.exports = { initGoogleSheets, getSheetsClient, initGoogleDrive, getDriveClient };
